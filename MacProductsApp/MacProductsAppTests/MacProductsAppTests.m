28bf9832c13a6f23d6ce43ab320b4cd0389c01b3
Robert DeNicola <thedizzle23@gmail.com>
2012-05-30 13:45:38 -0400

    final selection notice

---
+++ MacProductsApp/MacProductsAppTests/MacProductsAppTests.m
@@ -0,0 +1,32 @@
+//
+//  MacProductsAppTests.m
+//  MacProductsAppTests
+//
+//  Created by Robert DeNicola on 5/30/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "MacProductsAppTests.h"
+
+@implementation MacProductsAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MacProductsAppTests");
+}
+
+@end
